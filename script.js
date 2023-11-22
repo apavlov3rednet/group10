ef871a687dc5f7abe72a931633ec51cd12a4a621
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2023-11-22 15:02:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -109,3 +109,67 @@
     DataBase.setValue('user1', personal1.getUser());
 
 })(window);
+
+
+'use strict';
+
+var DOM = {
+    create: function(tagName, params = {}) {
+        if(!tagName)
+            return false;
+
+        let element = document.createElement(tagName);
+
+        if(params.className) {
+            element.classList.add(params.className);
+        }
+
+        if(typeof params.attrs === 'object') {
+            for(let i in params.attrs) {
+                element.setAttribute(i, params.attrs[i]);
+            }
+        }
+
+        if(typeof params.styles === 'object') {
+            for(let i in params.styles) {
+                element.style[i] = params.styles[i];
+            }
+        }
+
+        if(typeof params.children === 'array') {
+            params.children.forEach(item => {
+                element.append(item);
+            });
+        }
+
+        if(typeof params.events === 'object') {
+            for(let i in params.events) {
+                element.addEventListener(i, params.events[i]);
+            }
+        }
+
+        return element;
+    }
+}
+
+let test = DOM.create('div', {
+    className: "test",
+    styles: {
+        color: '#330000',
+        width: '100px',
+        height: '100px',
+        display: 'block',
+        backgroundColor: '#333'
+    },
+    children: [
+        DOM.create('span'),
+        DOM.create('b')
+    ],
+    events: {
+        click: function() {
+            console.log('ya');
+        }
+    }
+});
+
+document.body.append(test);
