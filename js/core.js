a419e83fb928e859036f4e4c4746b9e23e051405
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2023-12-09 18:48:39 +0300

    5

--- js/core.js
+++ js/core.js
@@ -1,60 +1,92 @@
-'use strict';
+"use strict";
 
-(function() {
-    //Получаем элементы разметки
-    let obOwnerForm = document.getElementById('owner');
-    let obCarForm = document.getElementById('car');
+(function () {
+  //Формы
+  let obOwnerForm = document.getElementById("owner");
+  let obCarForm = document.getElementById("car");
+  let obBrandForm = document.getElementById("brand");
 
-    //Получаем данные из БД
-    let arOwner = DB.get('owners') || [];
+  //Получаем данные из БД
+  let arOwner = DB.get("owners") || [];
+  let arBrand = DB.get("brands") || [];
 
-    function updateOwnerList() {
-        let selectOwner = obCarForm.querySelector('[name=OWNER]');
-        let childrens = [];
+  //Селекты
+  let selectOwner = obCarForm.querySelector("[name=OWNER]");
+  let selectBrand = obCarForm.querySelector("[name=BRAND]");
 
-        selectOwner.innerHTML = '';
+  function updateList(select, arr, title = "Выберите") {
+    let childrens = [];
 
-        childrens.push(DOM.create('option', {
-            attrs: {value: 0},
-            text: 'Выберите владельца'
-        }));
+    select.innerHTML = "";
 
-        arOwner.forEach(item => {
-            if(Object.keys(item).length > 0) {
-                childrens.push(DOM.create('option', {
-                    attrs: {value: item.id},
-                    text: item.params.NAME + ' ' + item.params.LAST_NAME
-                }));
-            }
-        });
+    childrens.push(
+      DOM.create("option", {
+        attrs: { value: 0 },
+        text: title,
+      })
+    );
 
-        DOM.adjust(selectOwner, {
-            children: childrens
-        });
-    }
-
-
-    obOwnerForm.addEventListener('submit', function(event) {
-        event.preventDefault(); //Отменяем штатное поведение на событие
-
-        let arFields = obOwnerForm.querySelectorAll('input');
-
-        let owner = new Model();
-
-        arFields.forEach(item => {
-            let params = {};
-            params[item.name] = item.value;
-            owner.set(params);
-        });
-
-        //Подготовка данных для БД
-        arOwner.push(owner);
-
-        //Сохранение в БД
-        DB.set('owners', arOwner);
-
-        updateOwnerList();
+    arr.forEach((item) => {
+      if (Object.keys(item).length > 0) {
+        childrens.push(
+          DOM.create("option", {
+            attrs: { value: item.id },
+            text: Object.values(item.params).join(" "),
+          })
+        );
+      }
     });
 
-    updateOwnerList();
+    DOM.adjust(select, {
+      children: childrens,
+    });
+  }
+
+  obOwnerForm.addEventListener("submit", function (event) {
+    event.preventDefault(); //Отменяем штатное поведение на событие
+
+    let arFields = obOwnerForm.querySelectorAll("input");
+
+    let owner = new Model();
+
+    arFields.forEach((item) => {
+      let params = {};
+      params[item.name] = item.value;
+      owner.set(params);
+    });
+
+    //Подготовка данных для БД
+    arOwner.push(owner);
+
+    //Сохранение в БД
+    DB.set("owners", arOwner);
+
+    updateList(selectOwner, arOwner);
+  });
+
+  obBrandForm.addEventListener("submit", function (event) {
+    event.preventDefault(); //Отменяем штатное поведение на событие
+
+    let arFields = obBrandForm.querySelectorAll("input");
+
+    let brand = new Model();
+
+    arFields.forEach((item) => {
+      let params = {};
+      params[item.name] = item.value;
+      brand.set(params);
+    });
+
+    //Подготовка данных для БД
+    arBrand.push(brand);
+
+    //Сохранение в БД
+    DB.set("brands", arBrand);
+
+    updateList(selectBrand, arBrand);
+  });
+
+  //Формирование селектов при первой загрузке
+  updateList(selectOwner, arOwner);
+  updateList(selectBrand, arBrand);
 })(window);
